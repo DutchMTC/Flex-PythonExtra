140c193e109d69ebd60f971346b1667e6e017222
Miyaki Truong <32574@ma-web.nl>
2021-11-05 14:14:38 +0100

    yeah

--- 04-DiscordBot/discord_bot.py
+++ 04-DiscordBot/discord_bot.py
@@ -1,6 +1,7 @@
 import discord
 
 client = discord.Client()
+
 @client.event
 async def on_ready():
     # De bot kan op veel verschillende servers draaien. Met deze regel code pak je de eerste server die deze bot heeft. Als jouw bot maar 1 server heeft is het makkelijk.
@@ -10,4 +11,19 @@ async def on_ready():
 
     # We printen de naam van de bot user uit.
     print(client.user, "has connected to the client")
+
+    channel = guild.text_channels[0]
+    print(channel.name, "is the name of the channel")
+    await channel.send("I'm online!")
+
+@client.event
+async def on_message(message):
+    print(message.channel.name, "the message was posted from this channel")
+    print(message.content)
+    print(message.author,"is the user who wrote the message")
+    print(message.created_at,"is when the message was posted")
+    print(message.channel,"is the channel this message was posted in")
+    if message.author.bot == False: 
+        await message.channel.send("Hello " + str(message.author))
+
 client.run("")
